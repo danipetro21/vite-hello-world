9a8dd907cd6a0273e8f6e3127dc1dc2e16a653b2
danipetro21 <47854779+danipetro21@users.noreply.github.com>
2022-12-10 21:11:42 +0100

    prova upload vite

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>La mia prima app con vite</h1>
+
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+  font-size: 50px;
+}
+</style>
